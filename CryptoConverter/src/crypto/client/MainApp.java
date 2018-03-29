8b6ca77e5be53d04f856a6e6836703e3bf0a71d6
Richard <Pahmp.richard@gmail.com>
2018-03-29 11:13:32 +0200

    Testing tabs and custom controls

---
+++ CryptoConverter/src/crypto/client/MainApp.java
@@ -0,0 +1,55 @@
+package crypto.client;
+
+import java.io.IOException;
+
+import crypto.client.view.ConverterPane;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private TabPane tabPane;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("CryptoConverter");
+		
+		initRootLayout();
+	}
+	
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			tabPane = new TabPane();
+			Tab tab = new Tab("Tab 1");
+			tab.setClosable(false);
+			loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("view/ConverterView.fxml"));
+			tab.setContent(loader.load());
+			tabPane.getTabs().add(tab);
+			rootLayout.setCenter(tabPane);
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
