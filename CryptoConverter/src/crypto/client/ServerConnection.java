fa859a01b1390e476e72d6698d4d6497cdea5bbd
Richard <Pahmp.richard@gmail.com>
2018-05-15 11:54:16 +0200

    Server thread now stops when closing.

--- CryptoConverter/src/crypto/client/ServerConnection.java
+++ CryptoConverter/src/crypto/client/ServerConnection.java
@@ -82,6 +82,15 @@ public class ServerConnection {
 		}
 	}
 	
+	public void closeConnection() {
+		try {
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void login(String user, String pass) {
 		LoginMessage message = new LoginMessage(user, pass);
 		try {
